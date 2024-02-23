4e090c92bd59f33be2f18febd20c085ce602814f
Hamed Jafari <hamedjix@gmail.com>
2024-02-22 19:42:22 -0500

    modification

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { Container, Typography } from "@material-ui/core";
+import React from "react";
 
 import CypressLogo from "../components/SvgCypressLogo";
 
@@ -7,23 +7,7 @@ export default function Footer() {
   return (
     <Container maxWidth="sm" style={{ marginTop: 50 }}>
       <Typography variant="body2" color="textSecondary" align="center">
-        Built by
-        <a
-          style={{ textDecoration: "none" }}
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://cypress.io"
-        >
-          <CypressLogo
-            style={{
-              marginTop: -2,
-              marginLeft: 5,
-              height: "20px",
-              width: "55px",
-              verticalAlign: "middle",
-            }}
-          />
-        </a>
+        Hamed Jafari
       </Typography>
     </Container>
   );
